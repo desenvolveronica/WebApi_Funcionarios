9ca8dba911a5a7495bfdfc0c154ef8d738e771f5
Verônica Helena <desenvolveronica@gmail.com>
2023-11-04 11:03:48 -0300

    Prop FuncionarioModel

--- WebApi_Funcionarios/Models/FuncionarioModel.cs
+++ WebApi_Funcionarios/Models/FuncionarioModel.cs
@@ -2,5 +2,13 @@
 {
     public class FuncionarioModel
     {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Departamento { get; set; }
+        public bool Ativo { get; set; }
+        public string Turno { get; set; }
+        public DateTime DataDeCriacao { get; set; } = DateTime.Now.ToLocalTime();
+        public DateTime DataDeAlteracao { get; set; } = DateTime.Now.ToLocalTime();
     }
 }
