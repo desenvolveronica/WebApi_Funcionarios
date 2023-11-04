b15c6ad79a3e2e12d7a2f91dadc2378ad5e30eff
Verônica Helena <desenvolveronica@gmail.com>
2023-11-04 12:40:11 -0300

    Criação da ConnectionStrings

---
+++ WebApi_Funcionarios/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi_Funcionarios.Models;
+
+namespace WebApi_Funcionarios.DataContext
+{//configuração para migration
+    public class ApplicationDbContext: DbContext //(DbContext) vem do NuGet EntityFrameworkCore
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<FuncionarioModel> Funcionarios { get; set; }//indicamos qual a model queremos transformar em tabela
+    }
+}
